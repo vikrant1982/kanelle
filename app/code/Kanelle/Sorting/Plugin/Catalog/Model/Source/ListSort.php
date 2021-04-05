b003ff4cd4f6d1d3471c1a0bdc4ec49483caa964
Vikrant Mishra <mishravikrant1982@gmail.com>
2021-04-06 02:28:13 +0530

    sorting issue

---
+++ app/code/Kanelle/Sorting/Plugin/Catalog/Model/Source/ListSort.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * Catalog Product List Sortable allowed sortable attributes source
+ *
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+namespace Magento\Catalog\Model\Config\Source;
+
+class ListSort
+{
+    public function afterToOptionArray(MagentoListSort $subject, array $result): array {
+
+    \array_push($result, ['label' => __('Name - A To Z'), 'value' => 'a_to_z']);
+    \array_push($result, ['label' => __('Name - Z To A'), 'value' => 'z_to_a']);
+    \array_push($result, ['label' => __('Price - Low To High'), 'value' => 'low_to_high']);
+    \array_push($result, ['label' => __('Price - High To Low'), 'value' => 'high_to_low']);
+
+
+    return $result;
+    }
+}
