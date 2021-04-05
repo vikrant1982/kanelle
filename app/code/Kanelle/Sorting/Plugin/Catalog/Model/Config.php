5a910d55f69314156a4060d944429db2e0da98a6
Vikrant Mishra <mishravikrant1982@gmail.com>
2021-04-06 01:13:56 +0530

    remove sortinng option

--- app/code/Kanelle/Sorting/Plugin/Catalog/Model/Config.php
+++ app/code/Kanelle/Sorting/Plugin/Catalog/Model/Config.php
@@ -3,6 +3,9 @@ namespace Kanelle\Sorting\Plugin\Catalog\Model;
 
 class Config {
   public function afterGetAttributeUsedForSortByArray(\Magento\Catalog\Model\Config $catalogConfig, $results){
+        unset($result['positionn']);
+        unset($result['price']);
+        unset($result['name']);
   	$results['a_to_z'] = __('Name - A To Z');
   	$results['z_to_a'] = __('Name - Z To A');
 	$results['low_to_high'] = __('Price - Low To High');
